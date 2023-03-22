fcdac3c0f1bb5d1637c9e81ee6cc350d8d5606b3
Berlinchik <davidarts11@gmail.com>
2023-03-22 19:17:36 +0200

    completed task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
-import Profile from "./Profile/Profile";
-import userObj from '../data/user.json'
 import PropTypes from 'prop-types'
-
+import Profile from "./Profile/Profile";
+import Statistics from "./Statistics/Statistics"
+import userObj from '../data/user.json'
+import data from '../data/data.json'
 
 export const App = () => {
 
@@ -10,6 +11,8 @@ export const App = () => {
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
+        gap: '50px',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
@@ -18,7 +21,14 @@ export const App = () => {
       }}
     >
       <Profile user={userObj} />
-      {/* <Statistics></Statistics> */}
+      <Statistics title="Upload stats" stats={data} colors={colors} />
     </div>
   );
 };
+
+function rundomColor() {
+    return '#' + (Math.floor(Math.random()*16777215).toString(16));
+}
+
+const colors = [1, 2, 3, 4, 5].map(color => rundomColor())
+
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,27 @@
+import './Statistics.css'
+import PropTypes from 'prop-types';
+
+const Statistics = ({ title, stats, colors }) => {
+    return <section className="statistics">
+        {title && <h2 className="title">{title}</h2>}
+        <ul className="stat-list">
+           {stats.map((stat, index) => <li className="item" key = {stat.id} style ={{backgroundColor: colors[index]}}  >
+      <span className="label">{stat.label}</span>
+      <span className="percentage">{stat.percentage}%</span>
+    </li>)}
+  </ul>
+</section>
+
+
+}
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        percentage: PropTypes.number.isRequired ,
+    })),
+    colors: PropTypes.array,
+ }
+
+export default Statistics;
